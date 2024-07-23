27d88abed569dcdcf7071c0d1da77be535d76b69
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-07-23 06:34:28 -0500

    Cleaned up definition and display of conjugation table

--- main.py
+++ main.py
@@ -3,36 +3,26 @@ from tkinter import *
 root = Tk()
 
 # Defining conjugation table
-en_infinitive = Entry(root)
-fr_infinitive = Entry(root)
-en_i = Entry(root)
-fr_i = Entry(root)
-en_you = Entry(root)
-fr_you = Entry(root)
-en_he_she = Entry(root)
-fr_he_she = Entry(root)
-en_we = Entry(root)
-fr_we = Entry(root)
-en_you_formal = Entry(root)
-fr_you_formal = Entry(root)
-en_they = Entry(root)
-fr_they = Entry(root)
+def define_conjugation_table():
+    con_list = [Label(root, text="to_speak")]  # Placeholder
+    con_subjects = ['Je', 'Tu', 'Il/Elle', 'Nous', 'Vous', 'Ils/Elles']
+    i = 0
+    for num in range(1, 14):
+        if num % 2 == 0:
+            con_list.append(Label(root, text=con_subjects[i]))
+            i += 1
+        else:
+            con_list.append(Entry(root))
+    return con_list
 
+
+conjugation_table = define_conjugation_table()
 # Display conjugation table
-en_infinitive.grid(column=0, row=0)
-fr_infinitive.grid(column=1, row=0)
-en_i.grid(column=0, row=1)
-fr_i.grid(column=1, row=1)
-en_you.grid(column=0, row=2)
-fr_you.grid(column=1, row=2)
-en_he_she.grid(column=0, row=3)
-fr_he_she.grid(column=1, row=3)
-en_we.grid(column=0, row=4)
-fr_we.grid(column=1, row=4)
-en_you_formal.grid(column=0, row=5)
-fr_you_formal.grid(column=1, row=5)
-en_they.grid(column=0, row=6)
-fr_they.grid(column=1, row=6)
-
+i = 0
+while i < len(conjugation_table) - 1:
+    for row in range(7):
+        for column in range(2):
+            conjugation_table[i].grid(column=column, row=row)
+            i += 1
 
 root.mainloop()
