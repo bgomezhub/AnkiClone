35e2ec263c7cbc6c551a7a0bc6467a01448cee02
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-07-23 05:15:53 -0500

    Defined conjugation table template

---
+++ main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+
+# Defining conjugation table
+en_infinitive = Entry(root)
+fr_infinitive = Entry(root)
+en_i = Entry(root)
+fr_i = Entry(root)
+en_you = Entry(root)
+fr_you = Entry(root)
+en_he_she = Entry(root)
+fr_he_she = Entry(root)
+en_we = Entry(root)
+fr_we = Entry(root)
+en_you_formal = Entry(root)
+fr_you_formal = Entry(root)
+en_they = Entry(root)
+fr_they = Entry(root)
+
+root.mainloop()
