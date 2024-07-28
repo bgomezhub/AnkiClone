b700310019724cfa52b6bf943719b611e4d945a0
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-07-28 11:10:19 -0500

    main file now only handles the mainloop and initial state of program.

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 from tkinter import *
-import ConjugationQuizPage as cqp
-from math import floor
+import Home
 
 # Define root widget
 root = Tk()
-f_conjugation_table = Frame(root)
-f_conjugation_table.grid(column=0, row=0, padx=200, pady=150)
-
-# conjugation to be
-conjugations = {'To be': ['Etre', 'suis', 'es', 'est', 'sommes', 'etes', 'sont']}  # Placeholder
-
-# Define & Display conjugation table
-conjugation_table = cqp.define_conjugation_table(f_conjugation_table)  # Create list of table labels/entries
-cqp.display_conjugation_table(conjugation_table)
-
-# Submission
-submit = Button(
-    f_conjugation_table, text="Submit",
-    command=lambda: cqp.submission(conjugations['To be'], conjugation_table, f_conjugation_table))
-submit.grid(column=0, row=8, columnspan=2,sticky='S', pady=20)  # Padding between table & button
-
+# Start Home Page
+Home.Home(root)
+# Tkinter loop
 root.mainloop()
