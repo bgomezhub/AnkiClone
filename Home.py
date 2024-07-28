539be3063a364ec20672de6802f200ebc5e89384
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-07-28 11:53:14 -0500

    Converted some variables to class variables to reduce parameters and arguments in functions.

--- Home.py
+++ Home.py
@@ -6,18 +6,17 @@ import ConjugationQuizPage
 class Home:
     def __init__(self, frame):
         # Title
-        f_home_title = Frame(frame)
-        f_home_title.pack(padx=200, pady=50)
-        Label(f_home_title, text='Welcome', font=('Arial', 30)).pack()
+        self.f_home_title = Frame(frame)
+        self.f_home_title.pack(padx=200, pady=50)
+        Label(self.f_home_title, text='Welcome', font=('Arial', 30)).pack()
         # Type of Quiz
-        f_home_options = Frame(frame)
-        f_home_options.pack(padx=200, pady=50)
-        Button(f_home_options, text='Conjugations',
-               command=lambda: self.submission([f_home_title, f_home_options, frame])).grid(column=0, row=0)
+        self.f_home_options = Frame(frame)
+        self.f_home_options.pack(padx=200, pady=50)
+        Button(self.f_home_options, text='Conjugations', command=lambda: self.submission(frame)).grid(column=0, row=0)
 
-    def submission(self, frames):
+    def submission(self, frame):
         # destroy all objects of home page
-        frames[0].destroy()
-        frames[1].destroy()
+        self.f_home_title.destroy()
+        self.f_home_options.destroy()
         # initialize objects for appropriate option clicked
-        ConjugationQuizPage.ConjugationQuizPage(frames[2])
+        ConjugationQuizPage.ConjugationQuizPage(frame)
