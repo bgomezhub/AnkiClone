fd0e609ceb6dd814c058ea9527728d31a0bd9c39
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-08-14 17:46:13 -0500

    Divided ManageDB.py by frames, added logic for sections.

--- ManageDB.py
+++ ManageDB.py
@@ -6,7 +6,7 @@ import sqlite3
 #conn = sqlite3.connect('en_fr_words.db')
 # Create cursor
 #c = conn.cursor()
-#c.execute("CREATE TABLE present_verb ( infinitive_en TEXT, infinitive_fr TEXT, i TEXT, you TEXT, he_she TEXT, we TEXT, you_formal TEXT, they TEXT)")
+#c.execute("CREATE TABLE nouns ( en TEXT, fr TEXT, gender TEXT, plural INTEGER)")
 
 #c.execute('DELETE FROM present_verb WHERE infinitive_en="to speak"')
 #c.execute('SELECT * FROM present_verb')
@@ -15,34 +15,58 @@ import sqlite3
 
 class ManageDB:
     def __init__(self):
-        # Define the page
+        # Define frames, section->table->recent
         self.root = Tk()
-        self.r_frame = Frame(self.root)
+        self.section_f = Frame(self.root)
+        self.section_f.grid(column=0, row=0)
+        self.table_f = Frame(self.root)
+        self.table_f.grid(column=0, row=1)
+        self.recent_f = Frame(self.root)
+        self.recent_f.grid(column=0, row=2)
+        # Initiate class variables
+        self.section()
         self.conjugation_table = []
-        self.db_manager()
-
-        # Submission button
-        Button(self.root, text="submit",
-              command=lambda: self.submission()).grid(columnspan=2, column=0, row=8, pady=10)
+        self.noun_table = []
+        self.con_manager()
 
         # Show recent inputs
         self.recent()
 
         self.root.mainloop()
 
-    def db_manager(self):
+    def con_manager(self):
         # Define db manager
         con_subjects = ["infinitive en", "infinitive fr", "I", "you", "he/she", "we", "you(formal)", "they"]
         for num in range(0, 16):
             if num % 2 == 0:
-                self.conjugation_table.append(Label(self.root, text=con_subjects[num//2], pady=8, padx=25))
+                self.conjugation_table.append(Label(self.table_f, text=con_subjects[num//2], pady=8, padx=25))
                 self.conjugation_table[num].grid(column=0, row=num//2)
             else:
-                self.conjugation_table.append(Entry(self.root))
+                self.conjugation_table.append(Entry(self.table_f))
                 self.conjugation_table[num].grid(column=1, row=num//2)
+        # Submission button
+        Button(self.table_f, text="submit",
+               command=lambda: self.sub_con()).grid(columnspan=2, column=0, row=8, pady=10)
+
+    def noun_manager(self):
+        #c.execute("CREATE TABLE nouns ( en TEXT, fr TEXT, gender TEXT, plural INTEGER)")
+        noun_req = ["English", "French", "Gender", "Plural"]
+        for num in range(0, 8):
+            if num % 2 == 0:
+                self.noun_table.append(Label(self.table_f, text=noun_req[num//2]))
+                self.noun_table[num].grid(column=0, row=num//2)
+            else:
+                self.noun_table.append(Entry(self.table_f))
+                self.noun_table[num].grid(column=1, row=num // 2)
+
+
+    def section(self):
+        conjugations = Button(self.section_f, text="conjugations",
+              command=lambda: self.con_manager()).grid(column=0, row=0, pady=10, sticky='NW')
+        nouns = Button(self.section_f, text="nouns",
+              command=lambda: self.noun_manager()).grid(column=1, row=0, pady=10, sticky='NW')
 
     def recent(self):
-        self.r_frame.grid(column=0, columnspan=2, row=9)
         # Connect to database
         conn = sqlite3.connect('en_fr_words.db')
         # Create cursor
@@ -52,14 +76,14 @@ class ManageDB:
         # Show recent inputs
         records = c.fetchmany(5)
         for record in records:
-            Label(self.r_frame, text=record, pady=5, padx=5).pack()
+            Label(self.recent_f, text=record, pady=5, padx=5).pack()
 
         # Commit changes and close
         conn.commit()
         conn.close()
         return
 
-    def submission(self):
+    def sub_con(self):
         # Connect to database
         conn = sqlite3.connect('en_fr_words.db')
         # Create cursor
@@ -84,10 +108,10 @@ class ManageDB:
         for item in self.conjugation_table:
             item.destroy()
         self.conjugation_table.clear()
-        self.db_manager()
+        self.con_manager()
 
         # Delete & update the recent widget
-        for widget in self.r_frame.winfo_children():
+        for widget in self.recent_f.winfo_children():
             widget.destroy()
         self.recent()
 
