98bfbc16d0ffc7f8d7bab18c140bcadc2a271b5e
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-08-16 14:04:01 -0500

    Added import to home.py to access NounQuizPage

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 from tkinter import *
 
 import ConjugationQuizPage
+import NounQuizPage
 
 
 class Home:
@@ -12,11 +13,17 @@ class Home:
         # Type of Quiz
         self.f_home_options = Frame(frame)
         self.f_home_options.pack(padx=200, pady=50)
-        Button(self.f_home_options, text='Conjugations', command=lambda: self.submission(frame)).grid(column=0, row=0)
+        Button(self.f_home_options, text='Conjugations',
+               command=lambda: self.submission(frame, 'con')).grid(column=0, row=0)
+        Button(self.f_home_options, text='Nouns',
+               command=lambda: self.submission(frame, 'nouns')).grid(column=1, row=0)
 
-    def submission(self, frame):
+    def submission(self, frame, option):
         # destroy all objects of home page
         self.f_home_title.destroy()
         self.f_home_options.destroy()
         # initialize objects for appropriate option clicked
-        ConjugationQuizPage.ConjugationQuizPage(frame)
+        if option == 'con':
+            ConjugationQuizPage.ConjugationQuizPage(frame)
+        elif option == 'nouns':
+            NounQuizPage.NounQuizPage(frame)
