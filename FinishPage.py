74569ef34fc231c5dbf3d0fe6244e3489184039a
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-08-21 22:02:40 -0500

    Added a FinishPage.py file to let the user know they have completed the quiz

---
+++ FinishPage.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+import Home
+
+
+class FinishPage:
+    def __init__(self, frame):
+        # Title
+        self.f_finish_title = Frame(frame)
+        self.f_finish_title.pack(padx=200, pady=50)
+        Label(self.f_finish_title, text=f'Congratulations you finished!', font=('Arial', 30)).pack()
+        # Type of Quiz
+        self.f_finish_options = Frame(frame)
+        self.f_finish_options.pack(padx=200, pady=50)
+
+        Button(self.f_finish_options, text='Quiz',
+               command=lambda: self.leave_page(frame)).grid(column=0, row=0)
+
+    def leave_page(self, frame):
+        for widget in frame.winfo_children():
+            widget.destroy()
+
+        return Home.Home(frame)
