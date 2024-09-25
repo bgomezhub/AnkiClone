e78eb7db5df1e24672e874af4e2422b6c669c674
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-09-24 23:34:21 -0500

    Removed hard coded font values from Home.py & replaced with CTkFont objects. Added fonts to __init__ function for display of the page.

--- Home.py
+++ Home.py
@@ -7,19 +7,24 @@ import SettingsPage
 
 class Home:
     def __init__(self, frame):
+        # Fonts
+        font = QuizManager.get_fonts()
+        self.font_title = ctk.CTkFont(family=font[0], size=font[1])
+        self.font_body = ctk.CTkFont(family=font[0], size=font[2])
         # Title
         self.f_home_title = ctk.CTkFrame(frame)
         self.f_home_title.pack(padx=200, pady=90)
-        ctk.CTkLabel(self.f_home_title, text='Welcome', font=QuizManager.get_title_font()).pack()
+        ctk.CTkLabel(self.f_home_title, text='Welcome', font=self.font_title).pack()
         # Type of Quiz
         self.f_home_options = ctk.CTkFrame(frame)
         self.f_home_options.pack(padx=200, pady=90)
-        ctk.CTkButton(self.f_home_options, text='Quiz', font=('Roboto', 20),
-                      command=lambda: self.submission(frame, 'quiz')).grid(column=0, row=0, pady=12)
-        ctk.CTkButton(self.f_home_options, text='Word Lists', font=('Roboto', 20),
-                      command=lambda: self.submission(frame, 'quiz')).grid(column=0, row=1, pady=12)
-        ctk.CTkButton(self.f_home_options, text='Settings', font=('Roboto', 20),
-                      command=lambda: self.submission(frame, 'settings')).grid(column=0, row=2, pady=12)
+        button_size = self.font_body["family"], self.font_body["size"] + 10
+        ctk.CTkButton(self.f_home_options, text='Quiz', font=button_size,
+                      command=lambda: self.submission(frame, 'quiz')).grid(column=0, row=0, pady=12, sticky='EW')
+        ctk.CTkButton(self.f_home_options, text='Word Lists', font=button_size,
+                      command=lambda: self.submission(frame, 'quiz')).grid(column=0, row=1, pady=12, sticky='EW')
+        ctk.CTkButton(self.f_home_options, text='Settings', font=button_size,
+                      command=lambda: self.submission(frame, 'settings')).grid(column=0, row=2, pady=12, sticky='EW')
 
     def submission(self, frame, option):
         # destroy all objects of home page
