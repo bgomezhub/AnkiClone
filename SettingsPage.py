b982534eec96c0366d7afc3fe47767a989a304bb
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-09-30 06:11:46 -0500

    In SettingsPage.py clicking a color button will now set the main color to the corresponding color of clicked button. Added new function reload_settings_page to update color values when changing main color or appearance.

--- SettingsPage.py
+++ SettingsPage.py
@@ -1,6 +1,4 @@
 import json
-from tkinter.ttk import Button
-
 import customtkinter as ctk
 import Home
 import QuizManager
@@ -31,7 +29,7 @@ class SettingsPage:
 
         # Return to Home
         ctk.CTkButton(self.f_submission, text='Done', font=self.font_body,
-                      command=lambda: self.submission(frame)).grid(column=0, row=0, sticky='N')
+                      command=lambda: self.submission()).grid(column=0, row=0, sticky='N')
 
     def options(self):
         # Open settings
@@ -169,11 +167,14 @@ class SettingsPage:
 
         # Write it into settings.json to be remembered on startup of program
         with open('settings.json', 'w') as file:
-            json.dump(settings, file)
+            json.dump(settings, file, indent=2)
         file.close()
 
         # Change the appearance now
         ctk.set_appearance_mode(settings['appearance'])
+
+        # Reload Page (for buttons)
+        self.reload_settings_page()
 
     def color_options(self):
         # Open settings
@@ -182,41 +183,90 @@ class SettingsPage:
         file.close()
 
         current_color = settings['color']
-        with open(f"themes/{current_color}.json", 'r') as file:
-            theme = json.load(file)
-        file.close()
-
         appearance = settings['appearance']
+
+        color_options = {"blue": ["#3a7ebf", "#1f538d"], "cyan": ["#00bbbb", "#00aaaa"],
+                         "green": ["#2CC985", "#2FA572"], "orange": ["#EE9F00", "#DD9F00"],
+                         "pink": ["#F8486D", "#F04660"], "purple": ["#682cc9", "#682fa5"],
+                         "red": ["#DD2222", "#DD1616"], "yellow": ["#CCCC22", "#CCCC0F"]}
+        c_o_keys = list(color_options.keys())
+
         # Set light or dark colors
         if appearance == 'light':
-            color_options = ['#3a7ebf', '#00bbbb', '#2CC985', '#EE9F00', '#F8486D', '#682cc9', '#DD2222', '#CCCC22']
-            current_color = theme['CTkButton']['fg_color'][0]
+            index = 0
         else:
-            color_options = ['#1f538d', '#00aaaa', '#2FA572', '#DD9F00', '#F04660', '#682fa5', '#DD1616', '#CCCC0F']
-            current_color = theme['CTkButton']['fg_color'][1]
+            index = 1
 
-        # Create buttons
-        i = 0
-        selected_button = []
-        for color in color_options:
-            temp = ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color,
-                                 command=lambda: self.set_color(color, selected_button), hover=False)
-            temp.grid(column=(i % 4), row=(i // 4 + 5), padx=1, pady=1, sticky='ew')
-
-            # Add border to current selected button
-            if color == current_color:
-                temp.configure(border_width=(self.font_body.cget("size")//10 + 2))
-                selected_button.append(temp)
-            i += 1
-
-    def set_color(self, color_selected, past_selected):
-        print(past_selected[0].cget("fg_color"))
+        # Should be in a loop. Could not get arguments to send of specific button. All returned info of last button.
+        ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[0]][index], hover=False,
+                      command=lambda: self.set_color(c_o_keys[0], current_color)).grid(column=0, row=5, padx=1, pady=1, sticky='ew')
+        ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[1]][index], hover=False,
+                      command=lambda: self.set_color(c_o_keys[1], current_color)).grid(column=1, row=5, padx=1, pady=1, sticky='ew')
+        ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[2]][index], hover=False,
+                      command=lambda: self.set_color(c_o_keys[2], current_color)).grid(column=2, row=5, padx=1, pady=1, sticky='ew')
+        ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[3]][index], hover=False,
+                      command=lambda: self.set_color(c_o_keys[3], current_color)).grid(column=3, row=5, padx=1, pady=1, sticky='ew')
+        ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[4]][index], hover=False,
+                      command=lambda: self.set_color(c_o_keys[4], current_color)).grid(column=0, row=6, padx=1, pady=1, sticky='ew')
+        ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[5]][index], hover=False,
+                      command=lambda: self.set_color(c_o_keys[5], current_color)).grid(column=1, row=6, padx=1, pady=1, sticky='ew')
+        ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[6]][index], hover=False,
+                      command=lambda: self.set_color(c_o_keys[6], current_color)).grid(column=2, row=6, padx=1, pady=1, sticky='ew')
+        ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[7]][index], hover=False,
+                      command=lambda: self.set_color(c_o_keys[7], current_color)).grid(column=3, row=6, padx=1, pady=1, sticky='ew')
         return
 
-    def submission(self, root_frame):
-        for widget in root_frame.winfo_children():
+    def set_color(self, new_color, past_color):
+        # Save info of last theme
+        with open(f"themes/{past_color}.json", 'r') as file:
+            old_theme = json.load(file)
+        file.close()
+        title_size = old_theme['CTkFont']['Windows']['title_size']
+        body_size = old_theme['CTkFont']['Windows']['size']
+
+        # Save to new selected theme
+        with open(f"themes/{new_color}.json", 'r') as file:
+            new_theme = json.load(file)
+        file.close()
+
+        new_theme['CTkFont']['Windows']['title_size'] = title_size
+        new_theme['CTkFont']['Windows']['size'] = body_size
+
+        with open(f"themes/{new_color}.json", 'w') as file:
+            json.dump(new_theme, file, indent=2)
+        file.close()
+
+        # Set the new selected theme
+        ctk.set_default_color_theme(f"themes/{new_color}.json")
+
+        # Set new theme color in settings
+        with open(f"settings.json", 'r') as file:
+            settings = json.load(file)
+        file.close()
+
+        settings["color"] = new_color
+
+        with open(f"settings.json", 'w') as file:
+            json.dump(settings, file, indent=2)
+        file.close()
+
+        # Reload page
+        self.reload_settings_page()
+
+        return
+
+    def reload_settings_page(self):
+        for widget in self.root_frame.winfo_children():
             widget.destroy()
 
-        Home.Home(root_frame)
+        SettingsPage(self.root_frame)
+
+        return
+
+    def submission(self):
+        for widget in self.root_frame.winfo_children():
+            widget.destroy()
+
+        Home.Home(self.root_frame)
 
         return
