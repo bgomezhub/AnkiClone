8ebada1ff64685dcba1fb9cf30ed4b476fb75da7
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2024-10-21 11:56:39 -0500

    SettingsPage.py, added "daily new word limit" option to the page. Changed placement of widgets due to daily new word addition. Renamed remove_font_entry to work with all entries.

--- SettingsPage.py
+++ SettingsPage.py
@@ -18,18 +18,19 @@ class SettingsPage:
         self.title.pack()
         # Handle Setting Options
         self.f_setting_options = ctk.CTkFrame(frame)
-        self.f_setting_options.pack(padx=200, pady=50)
+        self.f_setting_options.pack(padx=200, pady=25)
         self.title_spinbox = self.spinbox('title')
         self.body_spinbox = self.spinbox('body')
+        self.daily_spinbox = self.spinbox('daily')
         # Submission frame
         self.f_submission = ctk.CTkFrame(frame)
-        self.f_submission.pack(padx=200, pady=50)
+        self.f_submission.pack(padx=200)
 
         self.options()
 
         # Return to Home
         ctk.CTkButton(self.f_submission, text='Done', font=self.font_body,
-                      command=lambda: self.submission()).grid(column=0, row=0, sticky='N')
+                      command=lambda: self.submission()).grid(column=0, row=0, sticky='N', pady=25)
 
     def options(self):
         # Open settings
@@ -37,15 +38,20 @@ class SettingsPage:
             settings = json.load(file)
         file.close()
 
-        # Set font sizes
-        ctk.CTkLabel(self.f_setting_options, text="Title Font:", font=self.font_body).grid(column=0, row=0, columnspan=2)
-        self.title_spinbox.grid(column=2, row=0, pady=11, columnspan=2)
+        # Set daily new word limit
+        ctk.CTkLabel(self.f_setting_options, text="Daily New Word Limit:", font=self.font_body).grid(column=0, row=0,
+                                                                                           columnspan=2)
+        self.daily_spinbox.grid(column=2, row=0, pady=11, columnspan=2)
 
-        ctk.CTkLabel(self.f_setting_options, text="Body Font:", font=self.font_body).grid(column=0, row=1, columnspan=2)
-        self.body_spinbox.grid(column=2, row=1, pady=11, columnspan=2)
+        # Set font sizes
+        ctk.CTkLabel(self.f_setting_options, text="Title Font:", font=self.font_body).grid(column=0, row=1, columnspan=2)
+        self.title_spinbox.grid(column=2, row=1, pady=11, columnspan=2)
+
+        ctk.CTkLabel(self.f_setting_options, text="Body Font:", font=self.font_body).grid(column=0, row=2, columnspan=2)
+        self.body_spinbox.grid(column=2, row=2, pady=11, columnspan=2)
 
         # Toggle dark/light mode
-        ctk.CTkLabel(self.f_setting_options, text="Appearance (Dark/Light):", font=self.font_body).grid(column=0, row=2, columnspan=2)
+        ctk.CTkLabel(self.f_setting_options, text="Appearance (Dark/Light):", font=self.font_body).grid(column=0, row=3, columnspan=2)
         if settings['appearance'] == 'light':
             switch_var = ctk.StringVar(value="on")
         else:
@@ -53,15 +59,16 @@ class SettingsPage:
         appearance_switch = ctk.CTkSwitch(self.f_setting_options, text='', onvalue='on', offvalue='off',
                                           font=self.font_body, command=lambda: self.set_appearance_mode(switch_var),
                                           variable=switch_var)
-        appearance_switch.grid(column=2, row=2, pady=11, padx=10, columnspan=2)
+        appearance_switch.grid(column=2, row=3, pady=11, padx=10, columnspan=2)
 
         # Add spacing between appearance and color options
-        ctk.CTkLabel(self.f_setting_options, text='', font=self.font_body).grid(column=0, row=3, columnspan=4)
+        ctk.CTkLabel(self.f_setting_options, text='', font=self.font_body).grid(column=0, row=4, columnspan=4)
 
         # Set color of program
-        ctk.CTkLabel(self.f_setting_options, text="Color", font=self.font_body).grid(column=0, row=4, columnspan=4)
+        ctk.CTkLabel(self.f_setting_options, text="Color", font=self.font_body).grid(column=0, row=5, columnspan=4)
         self.color_options()
 
+        return
 
     def spinbox(self, font_type):
         # Create frame to house all widgets
@@ -74,21 +81,45 @@ class SettingsPage:
         # Input new size into entry
         if font_type == 'title':
             size_entry.insert(0, self.font_title.cget("size"))
-        else:
+        elif font_type == 'body':
             size_entry.insert(0, size_body)
+        else:
+            with open('settings.json', 'r') as file:
+                daily_word_limit = json.load(file)['daily_word_limit']
+            file.close()
+
+            size_entry.insert(0, daily_word_limit)
+            ctk.CTkButton(temp_frame, text="-", font=self.font_body,
+                          command=lambda: self.spinbox_daily_options(size_entry, -1),
+                          width=(size_body + 5)).grid(column=0, row=0)
+            ctk.CTkButton(temp_frame, text="+", font=self.font_body,
+                          command=lambda: self.spinbox_daily_options(size_entry, 1),
+                          width=(size_body + 2)).grid(column=3, row=0)
+
+            return temp_frame
 
         # Options placed on the sides, (-) == button, (+) == button2
         ctk.CTkButton(temp_frame, text="-", font=self.font_body,
-                      command=lambda: self.spinbox_options(font_type, size_entry, -1),
+                      command=lambda: self.spinbox_font_options(font_type, size_entry, -1),
                       width=(size_body + 5)).grid(column=0, row=0)
         ctk.CTkButton(temp_frame, text="+", font=self.font_body,
-                      command=lambda: self.spinbox_options(font_type, size_entry, 1),
+                      command=lambda: self.spinbox_font_options(font_type, size_entry, 1),
                       width=(size_body + 2)).grid(column=3, row=0)
-
 
         return temp_frame
 
-    def spinbox_options(self, font_type, size_entry, change):
+    def spinbox_daily_options(self, size_entry, change):
+        if change == -1:
+            size = int(size_entry.get()) - 1  # Decrease
+        else:
+            size = int(size_entry.get()) + 1  # Increase
+
+        # Saves value into daily limit
+        self.save_daily_limit(size)
+        # Remove current entry value and replace with new limit
+        self.remove_entry_value(size_entry, size)
+
+    def spinbox_font_options(self, font_type, size_entry, change):
         if change == -1:
             size = int(size_entry.get()) - 1  # Decrease
         else:
@@ -100,7 +131,7 @@ class SettingsPage:
         if font_type == 'body':
             self.update_body_values(size)
         # Remove current entry value and replace with new size
-        self.remove_font_entry(size_entry, size)
+        self.remove_entry_value(size_entry, size)
 
         return
 
@@ -128,10 +159,29 @@ class SettingsPage:
 
         return
 
+    def save_daily_limit(self, size):
+        # Open file
+        with open("settings.json", 'r') as file:
+            settings = json.load(file)
+        file.close()
+
+        # Change value
+        settings['daily_word_limit'] = size
+
+        # Write into file
+        with open("settings.json", 'w') as file:
+            json.dump(settings, file, indent=2)
+        file.close()
+
     def update_body_values(self, size):
         # Update font size
         self.font_body.configure(size=size)
         # Update widgets to scale with body font size (Entry to contain number and buttons to be semi-congruent to entry)
+        # Daily option
+        d_widgets = self.daily_spinbox.winfo_children()
+        d_widgets[1].configure(width=(size + 3))
+        d_widgets[2].configure(width=(size + 2))
+        d_widgets[0].configure(width=(size + 20))
         # body option
         b_widgets = self.body_spinbox.winfo_children()
         b_widgets[1].configure(width=(size + 3))  # (-) button is smaller in pixels than (+), scales proportionally for most font sizes
@@ -145,13 +195,14 @@ class SettingsPage:
 
         return
 
-    def remove_font_entry(self, font_entry, new_size):
+    def remove_entry_value(self, entry_w, new_value):
         # Remove current value of entry
-        font_entry.delete(0, last_index=ctk.END)
+        entry_w.delete(0, last_index=ctk.END)
         # Input new size into entry
-        font_entry.insert(0, new_size)
+        entry_w.insert(0, new_value)
 
         return
+
 
     def set_appearance_mode(self, switch_var):
         # Open settings
@@ -199,21 +250,21 @@ class SettingsPage:
 
         # Should be in a loop. Could not get arguments to send of specific button. All returned info of last button.
         ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[0]][index], hover=False,
-                      command=lambda: self.set_color(c_o_keys[0], current_color)).grid(column=0, row=5, padx=1, pady=1, sticky='ew')
+                      command=lambda: self.set_color(c_o_keys[0], current_color)).grid(column=0, row=6, padx=1, pady=1, sticky='ew')
         ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[1]][index], hover=False,
-                      command=lambda: self.set_color(c_o_keys[1], current_color)).grid(column=1, row=5, padx=1, pady=1, sticky='ew')
+                      command=lambda: self.set_color(c_o_keys[1], current_color)).grid(column=1, row=6, padx=1, pady=1, sticky='ew')
         ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[2]][index], hover=False,
-                      command=lambda: self.set_color(c_o_keys[2], current_color)).grid(column=2, row=5, padx=1, pady=1, sticky='ew')
+                      command=lambda: self.set_color(c_o_keys[2], current_color)).grid(column=2, row=6, padx=1, pady=1, sticky='ew')
         ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[3]][index], hover=False,
-                      command=lambda: self.set_color(c_o_keys[3], current_color)).grid(column=3, row=5, padx=1, pady=1, sticky='ew')
+                      command=lambda: self.set_color(c_o_keys[3], current_color)).grid(column=3, row=6, padx=1, pady=1, sticky='ew')
         ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[4]][index], hover=False,
-                      command=lambda: self.set_color(c_o_keys[4], current_color)).grid(column=0, row=6, padx=1, pady=1, sticky='ew')
+                      command=lambda: self.set_color(c_o_keys[4], current_color)).grid(column=0, row=7, padx=1, pady=1, sticky='ew')
         ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[5]][index], hover=False,
-                      command=lambda: self.set_color(c_o_keys[5], current_color)).grid(column=1, row=6, padx=1, pady=1, sticky='ew')
+                      command=lambda: self.set_color(c_o_keys[5], current_color)).grid(column=1, row=7, padx=1, pady=1, sticky='ew')
         ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[6]][index], hover=False,
-                      command=lambda: self.set_color(c_o_keys[6], current_color)).grid(column=2, row=6, padx=1, pady=1, sticky='ew')
+                      command=lambda: self.set_color(c_o_keys[6], current_color)).grid(column=2, row=7, padx=1, pady=1, sticky='ew')
         ctk.CTkButton(self.f_setting_options, text=' ', font=self.font_body, fg_color=color_options[c_o_keys[7]][index], hover=False,
-                      command=lambda: self.set_color(c_o_keys[7], current_color)).grid(column=3, row=6, padx=1, pady=1, sticky='ew')
+                      command=lambda: self.set_color(c_o_keys[7], current_color)).grid(column=3, row=7, padx=1, pady=1, sticky='ew')
         return
 
     def set_color(self, new_color, past_color):
