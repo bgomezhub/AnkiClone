f43ebcd12cc87eff414fa51b3d4700d624c38dce
Brayan Gomez <bjgomez2@mail.fhsu.edu>
2025-02-11 19:40:21 -0600

    Model.py, added argument types and return types to function declarations for improved readability.

--- Model.py
+++ Model.py
@@ -3,13 +3,14 @@ import json
 import sqlite3
 import random
 import datetime
+from customtkinter import CTkFrame
 
 # Import quiz Pages
 from pages import Finish
 from quiz import Noun, Adjective, Conjugation
 
 
-def get_fonts():
+def get_fonts() -> tuple[str, int, int]:
     """Return a tuple of font family, title size, body size."""
     settings = get_settings()
 
@@ -26,54 +27,54 @@ def get_fonts():
     return tuple((font_family, font_size_title, font_size_body))
 
 
-def get_settings():
+def get_settings() -> dict:
     """Return the settings json file."""
     with open("settings.json", 'r') as file:
-        settings = json.load(file)
+        settings: dict = json.load(file)
     file.close()
 
     return settings
 
 
-def set_settings(settings_file):
+def set_settings(settings: dict) -> None:
     """Write into settings.json file."""
     with open("settings.json", 'w') as file:
-        json.dump(settings_file, file, indent=2)
+        json.dump(settings, file, indent=2)
     file.close()
 
     return
 
 
-def get_theme():
+def get_theme() -> dict:
     """Return the current theme.json file."""
     color = get_settings()['color']
     with open(f"themes/{color}.json", 'r') as file:
-        theme = json.load(file)
+        theme: dict = json.load(file)
     file.close()
 
     return theme
 
 
-def get_theme_not_current(theme_color):
+def get_theme_not_current(theme_color: str) -> dict:
     """Return specified theme.json file."""
     with open(f"themes/{theme_color}.json", 'r') as file:
-        theme = json.load(file)
+        theme: dict = json.load(file)
     file.close()
 
     return theme
 
 
-def set_theme(theme_file):
+def set_theme(theme: dict) -> None:
     """Write into theme.json file."""
     color = get_settings()['color']
     with open(f"themes/{color}.json", 'w') as file:
-        json.dump(theme_file, file, indent=2)
+        json.dump(theme, file, indent=2)
     file.close()
 
     return
 
 
-def get_pts_cap(word, table):
+def get_pts_cap(word: str, table: str) -> bool:
     """Returns bool if pts_cap has been triggered."""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
@@ -84,7 +85,7 @@ def get_pts_cap(word, table):
     return pts_cap_value == 1
 
 
-def set_pts_cap(word, table):
+def set_pts_cap(word: str, table: str) -> None:
     """Sets pts_cap to True."""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
@@ -95,7 +96,7 @@ def set_pts_cap(word, table):
     return
 
 
-def remove_pts_cap(word, table):
+def remove_pts_cap(word: str, table: str) -> None:
     """Sets pts_cap to False."""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
@@ -106,7 +107,7 @@ def remove_pts_cap(word, table):
     return
 
 
-def get_new_info(word, table):
+def get_new_info(word: str, table: str) -> bool:
     """Returns bool if word is new."""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
@@ -117,7 +118,7 @@ def get_new_info(word, table):
     return new_value == 1
 
 
-def remove_new_from_word(word, table):
+def remove_new_from_word(word: str, table: str) -> None:
     """Sets new to false on word_info."""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
@@ -128,18 +129,18 @@ def remove_new_from_word(word, table):
     return
 
 
-def get_pts(word, table):
+def get_pts(word: str, table: str) -> int:
     """Returns pts of selected word"""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
     c.execute(f"SELECT pts FROM word_info WHERE word = '{word}' and type = '{table}'")
-    pts = c.fetchone()[0]
+    pts: int = c.fetchone()[0]
     conn.close()  # Close db connection
 
     return pts
 
 
-def set_pts(word, table, word_pts, gained_pts):
+def set_pts(word: str, table: str, word_pts: int, gained_pts: int) -> None:
     """Adds pts to word_info."""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
@@ -153,12 +154,12 @@ def set_pts(word, table, word_pts, gained_pts):
     return
 
 
-def calculate_pts(grade):
+def calculate_pts(grade: int) -> int:
     """Returns pts based of grade of submission."""
     return grade * 20 - 10  # +- 10 pts, 50% results in 0
 
 
-def update_pts(word, table, grade):
+def update_pts(word: str, table: str, grade: int) -> None:
     """Updates pts & sets pts_cap."""
     pts_gained = calculate_pts(grade)
     word_pts = get_pts(word, table)
@@ -168,7 +169,7 @@ def update_pts(word, table, grade):
     return
 
 
-def set_cooldown(word, table, date):
+def set_cooldown(word: str, table: str, date: str) -> None:
     """Sets next review date for word."""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
@@ -179,7 +180,7 @@ def set_cooldown(word, table, date):
     return
 
 
-def calculate_cooldown_days(pts):
+def calculate_cooldown_days(pts: int) -> int:
     """Returns # of days until next review date."""
     if pts < 15:
         return 1
@@ -197,15 +198,15 @@ def calculate_cooldown_days(pts):
         return 13
 
 
-def calculate_cooldown_date(days):
+def calculate_cooldown_date(days: int) -> str:
     """Returns the date until next review."""
-    cd = datetime.datetime.now() + datetime.timedelta(days=days)  # Add cooldown to today's date
-    date = cd.strftime("%Y%m%d")  # format YYYYMMDD
+    cd: datetime = datetime.datetime.now() + datetime.timedelta(days=days)  # Add cooldown to today's date
+    date: str = cd.strftime("%Y%m%d")  # format YYYYMMDD
 
     return date
 
 
-def update_cooldown(word, table):
+def update_cooldown(word: str, table: str) -> None:
     """Updates cooldown date & removes pts_cap of word."""
     pts = get_pts(word, table)
     days = calculate_cooldown_days(pts)
@@ -213,13 +214,15 @@ def update_cooldown(word, table):
     set_cooldown(word, table, date)
     remove_pts_cap(word, table)
 
+    return
 
-def get_composite(table):
+
+def get_composite(table: str) -> bool:
     """Returns bool if word is a composite verb."""
     return table in ['passe_compose', 'futur_anterieur', 'plus_que_parfait']
 
 
-def get_composite_verbs(word):
+def get_composite_verbs(word: str) -> list[str]:
     """Returns alternating list of aux and verb."""
     infinitive_verb = get_word(word[0], word[2])[1]
     aux = get_word(word[1], word[2])
@@ -235,23 +238,23 @@ def get_composite_verbs(word):
     return composite
 
 
-def get_noun(table):
+def get_noun(table: str) -> bool:
     """Returns bool if word is a noun."""
     return table == 'noun'
 
 
-def get_word(word, table):
+def get_word(word: str, table: str) -> tuple:
     """Returns word properties of word."""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
     c.execute(f"SELECT * FROM {table} WHERE en = '{word}'")
-    word_properties = c.fetchone()
+    word_properties: tuple = c.fetchone()
     conn.close()  # Close db connection
 
     return word_properties
 
 
-def get_questions():
+def get_questions() -> list[str]:
     """Returns word list of due & new words."""
     conn = sqlite3.connect('en_fr_words.db')  # Connect to database
     c = conn.cursor()  # Create cursor
@@ -264,12 +267,14 @@ def get_questions():
     # Select due words
     '''c.execute(f"Select word, type FROM word_info "
               f"WHERE cooldown <= {datetime.datetime.now().strftime('%Y%m%d')} AND new = 0")'''
-    c.execute("SELECT word, type FROM word_info "
-              "WHERE word = 'to eat' and type = 'passe_compose'")
+    '''c.execute("SELECT word, type FROM word_info "
+              "WHERE word = 'to eat' and type = 'passe_compose'")'''
     '''c.execute("SELECT word, type FROM word_info "
               "WHERE word = 'to speak'")'''
     '''c.execute("SELECT word, type FROM word_info "
                   "WHERE word = 'big, tall'")'''
+    '''c.execute("SELECT word, type FROM word_info "
+              "WHERE word = 'time'")'''
 
     word_list = c.fetchmany(1)
     print(word_list)
@@ -279,14 +284,14 @@ def get_questions():
     return word_list
 
 
-def next_question(f_root, word_list):
+def next_question(f_root: CTkFrame, word_list: list[str]):
     """Randomizes next question and sends program to page depending on the type of the word."""
     # Base condition
     if len(word_list) == 0:
         return Finish.Finish(f_root)
 
-    selected_index = random.randint(0, len(word_list) - 1)  # Randomize next word
-    word_table = word_list[selected_index][1]
+    selected_index: int = random.randint(0, len(word_list) - 1)  # Randomize next word
+    word_table: str = word_list[selected_index][1]
 
     # Send to corresponding page
     if word_table == 'noun':
@@ -299,8 +304,8 @@ def next_question(f_root, word_list):
     return
 
 
-def remove_question(word_list, remove_from_word_list=True):
-    """Returns word_list with current question unselected or removed."""
+def remove_question(word_list: list[str, int], remove_from_word_list: bool=True) -> list[str]:
+    """Returns word_list with current question unselected or removed and index removed."""
     if remove_from_word_list:
         selected_index = word_list[1]
         word_list = word_list[0]
